0638ac9ca3dd71df8bb791d4b3df4d1eb14a3e2a
Masihghaznavi <42281679+MasihGhaznavi@users.noreply.github.com>
2018-08-11 00:30:17 +0430

    Add files via upload

---
+++ pwm-Final.py
@@ -0,0 +1,384 @@
+  #-------------------------------------#
+  #Auter:MASIH,GHAZNAVI
+  #Name_program(PWM)
+  #Bin:Python
+  #Chanel-Telegram-Auter:@Masih_PC
+
+  #ID-Telegram-Auter:@MasihGhaznavi
+  #Version: FinaL 2.1
+  #-------------------------------------#
+#library
+from colorama import init
+from termcolor import colored
+import random
+
+#Start
+init()
+print(colored('''
+                              ------------------------ 
+                              |..::Welcome To PWM::..|
+                              ------------------------
+
+                            +/+/+/+/+/+/+/+/+/+/+/+/+/+
+                            | .:Version: FinaL 2.1:.  |
+                            +/+/+/+/+/+/+/+/+/+/+/+/+/+
+
+                          ******************************* 
+                         |           .:Help:.            |  
+                         + 1-results anuoncment-(R)      +
+                         +   2-Show List Name(show)      +
+                         +    3-Win(min 1/max 20)        +
+                         +      4-Name_List(~)           +
+                          *******************************''',"green"))
+#input
+List_Name=[]
+while True:
+    name=input("-+-+-+-+-+-+>>>")
+    	 
+    List_Name.append(name)
+    if name == 'R' :
+      List_Name.remove('R')
+      break
+    elif name == 'show' :
+      print("\n\n")
+      List_Name.remove('show')
+      print(List_Name)
+      print("\n\n")
+      break
+#_______________________________________________________#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+print("\n")
+init
+print("..::""(1)",random_Name,"::..")
+#-------------------#
+del random_Name
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(2)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-------------------# 
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(3)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(4)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(5)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(6)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(7)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(8)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(9)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(10)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(11)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(12)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(13)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(14)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(15)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(16)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(17)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	prin ("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(18)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(19)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#------------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+random_generator = random.SystemRandom()     
+random_Name=random_generator.choice(List_Name)
+so=(input("Do you want to continue? (y/n)"))
+print("\n")
+if so == "y" :
+  print("..::""(20)",random_Name,"::..")
+  del random_Name
+elif so == "n":
+  print(" BY BY ! ")
+  exit()
+#-----------------#
+else :
+	print("""                       
+(O_o) ..::Erorr::.. (O_o)
+      .:Enter a y/n:.
+""")
+#-----------------#
+
+#-----------------
+#_______________________________________________________#
+
+#Ouput
+input()
